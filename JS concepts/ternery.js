4316e0cc18d80d38d30fa6bc45d63f6808b94afd
shashank-905 <mnshashank905@gmail.com>
2026-07-21 21:02:19 +0530

    full if concept

---
+++ JS concepts/ternery.js
@@ -0,0 +1,7 @@
+// ternery operator:
+// condition ? codeiftrue:codeiffalse
+
+// ex:
+let age = 22;
+let message = age >= 18 ? "u can have voter id":"u are not enough metured to have voter id";
+console.log(message);
