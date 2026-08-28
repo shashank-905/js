836dfc318e7ffbe3f4b1566619910de4f70fbbc3
shashank-905 <mnshashank905@gmail.com>
2026-08-28 21:15:16 +0530

    added temprature conversion

--- JS projects/temp_converter/tc.js
+++ JS projects/temp_converter/tc.js
@@ -1,3 +1,21 @@
-function convert(){
+const textbox=document.getElementById("textbox");
+const toFahrenheit=document.getElementById("toFahrenheit");
+const toCelsius=document.getElementById("toCelsius");
+const result=document.getElementById("result");
+let temp;
 
+function convert(){
+    if(toFahrenheit.checked){
+        temp=Number(textbox.value);
+        temp=((9/5)*temp)+32;
+        result.textContent=temp.toFixed(1)+"Fahrenheit";
+    }
+    else if(toCelsius.checked){
+       temp=Number(textbox.value);
+       temp=5/9(temp-32);
+        result.textContent=temp.toFixed(1)+"Celsius";
+    }
+    else{
+        result.textContent="select a unit";
+    }
 }
